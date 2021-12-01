35001caaa4a0d7be90d5b5c686c59470d510502d
Noriko Tsutsui <isandesuyo@gmail.com>
2021-12-01 23:12:16 +0900

    線を引く

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
 	"log"
+	"math"
 	"os"
 )
 
@@ -22,10 +24,39 @@ var (
 	linecolor color.Color = color.RGBA{0, 0, 0, 0}
 )
 
-func main() {
+// 2点を受け取って線を引く
+func drawLine(m *image.RGBA, x1, y1, x2, y2 float64) {
+	dx := x2 - x1
+	dy := y2 - y1
 
+	// 距離
+	length := math.Sqrt(dx*dx + dy*dy)
+	fmt.Println("length: ", length)
+
+	// ラジアン
+	radian := math.Atan2(dy, dx)
+	fmt.Println("radian: ", radian)
+	var deg = radian * 180 / math.Pi
+	fmt.Printf("角度：%f\n", deg)
+
+	for l := 0.0; l < length; l++ {
+		// x座標とy座標を計算
+		x := x1 + l*math.Cos(radian)
+		y := y1 + l*math.Sin(radian)
+		fmt.Printf("(x,y): (%f,%f)\n", x, y)
+
+		// ビットマップ外の点は描写しない
+		if (x >= 0 && int(x) < width) && (y >= 0 && int(y) < height) {
+			m.Set(int(x), int(y), linecolor)
+		}
+	}
+}
+
+func main() {
 	m := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(m, m.Bounds(), &image.Uniform{bgcolor}, image.ZP, draw.Src)
+
+	drawLine(m, 10, 10, 400, 450)
 
 	file, err := os.Create(filename)
 	if err != nil {
