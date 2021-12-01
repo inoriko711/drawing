5f4ea94d12b0afbc7918f9a4e5d20698538c6b2d
Noriko Tsutsui <isandesuyo@gmail.com>
2021-12-02 00:12:23 +0900

    内側の頂点

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func drawLine(m *image.RGBA, x1, y1, x2, y2 float64) {
 	}
 }
 
-func pentagon() []Points {
+func pentagonOutside() []Points {
 	// 頂点の数
 	vertexNum := 5.0
 
@@ -79,15 +79,40 @@ func pentagon() []Points {
 	return points
 }
 
+func pentagonInside() []Points {
+	// 頂点の数
+	vertexNum := 5.0
+
+	// 中心から頂点までの距離
+	R := width / 4.0
+	radian := math.Pi / vertexNum
+
+	// 中心のx,y座標
+	centerX := width / 2.0
+	centerY := height / 2.0
+
+	var points []Points
+	for i := 1.0; i < vertexNum*2; i = i + 2 {
+		x := centerX + R*math.Cos(radian*i)
+		y := centerY + R*math.Sin(radian*i)
+		points = append(points, Points{x, y})
+	}
+
+	return points
+}
 func main() {
 	m := image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
 	draw.Draw(m, m.Bounds(), &image.Uniform{bgcolor}, image.ZP, draw.Src)
 
-	points := pentagon()
-	for _, p := range points {
+	pointsOutside := pentagonOutside()
+	for _, p := range pointsOutside {
 		drawLine(m, 250, 250, p.X, p.Y)
 	}
 
+	pointsInside := pentagonInside()
+	for _, p := range pointsInside {
+		drawLine(m, 250, 250, p.X, p.Y)
+	}
 	file, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
