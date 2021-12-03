2c2076cb2b185f1bbbb1e4f06842ae3bf9fe274a
Noriko Tsutsui <isandesuyo@gmail.com>
2021-12-03 22:37:22 +0900

    先は最後に一気に書き上げるようにする

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
@@ -27,9 +26,14 @@ var (
 	linecolor color.Color = color.RGBA{0, 0, 0, 0}
 )
 
-type Points struct {
+type Point struct {
 	X float64
 	Y float64
+}
+
+type DrawPoint struct {
+	X int
+	Y int
 }
 
 func init() {
@@ -37,36 +41,34 @@ func init() {
 	centerY = height / 2.0
 }
 
-// 2点を受け取って線を引く
-func drawLine(m *image.RGBA, x1, y1, x2, y2 float64) {
+// 2点を受け取ってその間の座標を返す
+func getCoordinates(m *image.RGBA, x1, y1, x2, y2 float64) []DrawPoint {
 	dx := x2 - x1
 	dy := y2 - y1
 
 	// 距離
 	length := math.Sqrt(dx*dx + dy*dy)
-	fmt.Println("length: ", length)
 
 	// ラジアン
 	radian := math.Atan2(dy, dx)
-	fmt.Println("radian: ", radian)
-	var deg = radian * 180 / math.Pi
-	fmt.Printf("角度：%f\n", deg)
 
+	var points []DrawPoint
 	for l := 0.0; l < length; l++ {
 		// x座標とy座標を計算
 		x := x1 + l*math.Cos(radian)
 		y := y1 + l*math.Sin(radian)
-		// fmt.Printf("(x,y): (%f,%f)\n", x, y)
 
 		// ビットマップ外の点は描写しない
 		if (x >= 0 && x < width) && (y >= 0 && y < height) {
-			m.Set(int(x), int(y), linecolor)
+			points = append(points, DrawPoint{X: int(x), Y: int(y)})
 		}
 	}
+
+	return points
 }
 
 // 星の外側の頂点座標を求める
-func pentagonOutside() []Points {
+func pentagonOutside() []Point {
 	// 頂点の数
 	vertexNum := 5.0
 
@@ -76,18 +78,18 @@ func pentagonOutside() []Points {
 	// 中心から頂点への直線同士が成す角度
 	radian := math.Pi * 2 / vertexNum
 
-	var points []Points
+	var points []Point
 	for i := 0.0; i < vertexNum; i++ {
 		x := centerX + R*math.Cos(radian*i)
 		y := centerY + R*math.Sin(radian*i)
-		points = append(points, Points{x, y})
+		points = append(points, Point{x, y})
 	}
 
 	return points
 }
 
 // 星の内側の頂点座標を求める
-func pentagonInside() []Points {
+func pentagonInside() []Point {
 	// 頂点の数
 	vertexNum := 5.0
 
@@ -97,11 +99,11 @@ func pentagonInside() []Points {
 	// 外側の1/2の角度を求める
 	radian := math.Pi / vertexNum
 
-	var points []Points
+	var points []Point
 	for i := 1.0; i < vertexNum*2; i = i + 2 { // 外側と角度をずらしつつ幅を揃える
 		x := centerX + R*math.Cos(radian*i)
 		y := centerY + R*math.Sin(radian*i)
-		points = append(points, Points{x, y})
+		points = append(points, Point{x, y})
 	}
 
 	return points
@@ -111,19 +113,24 @@ func main() {
 	m := image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
 	draw.Draw(m, m.Bounds(), &image.Uniform{bgcolor}, image.ZP, draw.Src)
 
-	pointsOutside := pentagonOutside()
+	var drawPoints []DrawPoint
 
-	pointsInside := pentagonInside()
+	pOutside := pentagonOutside()
+	pInside := pentagonInside()
 
-	for i := 0; i < len(pointsOutside); i++ {
-		drawLine(m, pointsOutside[i].X, pointsOutside[i].Y, pointsInside[i].X, pointsInside[i].Y)
+	for i := 0; i < len(pOutside); i++ {
+		drawPoints = append(drawPoints, getCoordinates(m, pOutside[i].X, pOutside[i].Y, pInside[i].X, pInside[i].Y)...)
 
-		if i+1 == len(pointsOutside) {
+		if i+1 == len(pOutside) {
 			// pointsInsideの最後の点はpointsOutsideの最初の点につなげる
-			drawLine(m, pointsInside[i].X, pointsInside[i].Y, pointsOutside[0].X, pointsOutside[0].Y)
+			drawPoints = append(drawPoints, getCoordinates(m, pInside[i].X, pInside[i].Y, pOutside[0].X, pOutside[0].Y)...)
 		} else {
-			drawLine(m, pointsInside[i].X, pointsInside[i].Y, pointsOutside[i+1].X, pointsOutside[i+1].Y)
+			drawPoints = append(drawPoints, getCoordinates(m, pInside[i].X, pInside[i].Y, pOutside[i+1].X, pOutside[i+1].Y)...)
 		}
+	}
+
+	for _, points := range drawPoints {
+		m.Set(points.X, points.Y, linecolor)
 	}
 
 	file, err := os.Create(filename)
