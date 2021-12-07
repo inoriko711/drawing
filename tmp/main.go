f8d1987068e339500221f289bc9e16ca49327b99
Noriko Tsutsui <isandesuyo@gmail.com>
2021-12-08 01:37:38 +0900

    ライン画像

--- tmp/main.go
+++ tmp/main.go
@@ -12,8 +12,10 @@ import (
 func main() {
 	m := image.NewRGBA(image.Rect(0, 0, 11, 13))
 	draw.Draw(m, m.Bounds(), &image.Uniform{color.RGBA{255, 255, 255, 255}}, image.Point{}, draw.Src)
-	m.Set(5, 6, color.RGBA{0, 0, 0, 0})
-	file, err := os.Create("point.png")
+	for x := 1; x < 10; x++ {
+		m.Set(x, 6, color.RGBA{0, 0, 0, 0})
+	}
+	file, err := os.Create("line.png")
 	if err != nil {
 		log.Fatal(err)
 	}
